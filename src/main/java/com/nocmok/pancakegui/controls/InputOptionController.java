cf92f19e9e667b539eae7054549056e8041e39a6
Nikolai Safonov <nyusafonov@edu.hse.ru>
2021-04-11 17:55:15 +0300

    input option added

---
+++ src/main/java/com/nocmok/pancakegui/controls/InputOptionController.java
@@ -0,0 +1,48 @@
+package com.nocmok.pancakegui.controls;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class InputOptionController extends OptionControllerBase<String> {
+
+    public static InputOptionController getNew() {
+        return ControllerBase.getNew("input_option_layout.fxml");
+    }
+
+    @FXML
+    private Label optionName;
+
+    @FXML
+    private TextField textField;
+
+    private Parent root;
+
+    @Override
+    public String getSelected() {
+        return textField.getText();
+    }
+
+    @Override
+    protected void setRoot(Parent root) {
+        this.root = root;
+    }
+
+    @Override
+    public Parent root() {
+        return root;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void setOptionName(String name){
+        optionName.setText(name);
+    }
+}
