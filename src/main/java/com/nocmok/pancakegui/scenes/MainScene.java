c8a0d5e3136f56ca82588e4f01fce8815f5a0d80
Nikolai Safonov <nyusafonov@edu.hse.ru>
2021-04-12 12:39:20 +0300

    rewriten with controllers

--- src/main/java/com/nocmok/pancakegui/scenes/MainScene.java
+++ src/main/java/com/nocmok/pancakegui/scenes/MainScene.java
@@ -1,12 +1,7 @@
 package com.nocmok.pancakegui.scenes;
 
-import java.io.File;
-import java.io.IOException;
+import com.nocmok.pancakegui.controls.MainSceneController;
 
-import com.nocmok.pancakegui.PancakeApp;
-import com.nocmok.pancakegui.controls.ImageExplorer;
-
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -15,24 +10,10 @@ public class MainScene extends Scene {
 
     private static final Parent placeholder = new Label("failed to load layout");
 
-    private ImageExplorer imgExplorer;
-
     public MainScene() {
         super(placeholder);
-        
-        Parent root = null;
-        try {
-            root = FXMLLoader.load(PancakeApp.getLayout("main_scene_layout.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        setRoot(root);
-
-        imgExplorer = (ImageExplorer) lookup("#imageExplorer");
-        imgExplorer.setPlaceholder(new Label("no image to display"));
-        imgExplorer.setImage(new File("C:\\Users\\uporo\\source\\java\\pancake-desktop\\pleades1-crop-hpfm.png"));
+        MainSceneController controller = MainSceneController.getNew();
+        setRoot(controller.root());
     }
 
 }
